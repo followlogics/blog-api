61eab729255eb4243aadd7d4619ce1251d90a47d
vikas <ecom_5@sblsoftware.com>
2018-02-21 19:24:54 +0530

    migration

---
+++ database/migrations/2018_02_21_090508_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->addColumn('string','email',array('length'=>51));
+            $table->addColumn('text','name');
+            $table->addColumn('text','address');
+            $table->addColumn('date','dob');
+            $table->addColumn('string','password',array('length'=>32));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

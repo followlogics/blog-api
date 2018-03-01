594a10ce1e7788080209c1a5aefa68f5c33854c0
vikas <ecom_5@sblsoftware.com>
2018-03-01 19:08:51 +0530

    detect content change

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -110,6 +110,7 @@ echo '&#8377; &#x20b9; Testing :' . ($name);
     <div id="content">
         This is the content
     </div>
+    <button type="button" id="addContent">Add Content</button>
     <script>
         function processAjaxData(response, urlPath) {
             document.getElementById("content").innerHTML = response.html;
@@ -122,5 +123,42 @@ echo '&#8377; &#x20b9; Testing :' . ($name);
                 document.title = e.state.pageTitle;
             }
         };
+        /* DOM content change detect */
+        window.Virus = {
+            starts: function () {
+                 
+                if (typeof document.body.addEventListener != 'undefined') {
+                   document.body.addEventListener('DOMSubtreeModified', function () {
+                        console.log('Content Cahnge on virus', new Date());
+                    }, false);
+                } else if (typeof document.body.attachEvent != 'undefined') {
+                    document.body.attachEvent('DOMSubtreeModified', function () {
+                        document.title = 'Changed at ' + new Date();
+                        console.log('Content Cahnge on virus', new Date());
+                    }, false);
+                }
+                var customEvent = new Event("infected");
+                var content = document.getElementById('content');
+                var btn = document.getElementById('addContent');
+                btn.addEventListener('click', function () {
+                    content.innerHTML ='click on blog add contemt button';
+                    content.dispatchEvent(customEvent);
+                });
+                content.addEventListener('infected', function () {
+                    console.log('Virus infected')
+                });
+            }
+        };
+        Virus.starts();
+        MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+        var observer = new MutationObserver(function (mutations, observer) {
+            console.log(mutations, observer);
+        });
+
+        observer.observe(document, {
+            subtree: true,
+            attributes: true
+        });
     </script>
 </body>

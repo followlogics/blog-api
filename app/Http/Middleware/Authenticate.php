dac7845755eda4d2919f28853071af276e7dda96
vikas <ecom_5@sblsoftware.com>
2018-04-19 19:32:18 +0530

    cros setup

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,8 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
 
-class Authenticate
-{
+class Authenticate {
     /**
      * The authentication guard factory instance.
      *
@@ -20,8 +19,7 @@ class Authenticate
      * @param  \Illuminate\Contracts\Auth\Factory  $auth
      * @return void
      */
-    public function __construct(Auth $auth)
-    {
+    public function __construct(Auth $auth) {
         $this->auth = $auth;
     }
 
@@ -33,12 +31,32 @@ class Authenticate
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
-    {
+    public function handle($request, Closure $next, $guard = null) {
         if ($this->auth->guard($guard)->guest()) {
-            return response(json_encode(array('error'=>'Unauthorized.')), 401);
+            return response(json_encode(array('error' => 'Unauthorized.')), 401);
         }
-
-        return $next($request);
+        /* add Allowed Domain*/
+        $allowedDomains = array("http://localhost:8100");
+        $origin = $request->server('HTTP_ORIGIN');
+        if (in_array($origin, $allowedDomains)) {
+            //Intercepts OPTIONS requests
+            if ($request->isMethod('OPTIONS')) {
+                $response = response('', 200);
+            } else {
+                // Pass the request to the next middleware
+                $response = $next($request);
+            }
+            // Adds headers to the response
+            $response->header('Access-Control-Allow-Origin', '*');
+            $response->header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+            $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+        }
+        return $response;
+       /* add Allowed Domain*/
+        
+       /* otherwise simply return $next($request);
+        * 
+        */
     }
+
 }

251fcf7f33014e375f159bf9ddab1baca7e7541c
Vikas Kumar <VikashAmbani@users.noreply.github.com>
2019-06-07 13:25:21 +0530

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,19 +12,28 @@ use App\User;
 class DashboardController extends Controller {
 
     function __construct() {
-       // $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     public function index(Request $request) {
-           return view('dashboard.index',['isAjax'=>FALSE]);
-   
+
+        if ($request->ajax()) {
+            $view = view('dashboard.index',['isAjax'=>TRUE])->render();
+            return response()->json(["status" => 'success','html' => $view, 'title' => 'Dashboard']);
+        } else {
+            return view('dashboard.index',['isAjax'=>FALSE]);
+        }
     }
     
     public function profile(Request $request) {
         $user = $request->user();
         $user = User::where('id', $user->user_id)->first()->toArray();
-       
+        if ($request->ajax()) {
+            $view = view('profile.index', ['isAjax' => TRUE, 'user' => $user])->render();
+            return response()->json(["status" => 'success', 'html' => $view, 'title' => 'Profile']);
+        } else {
             return view('profile.index', ['isAjax' => FALSE, 'user' => $user]);
+        }
     }
 
 }

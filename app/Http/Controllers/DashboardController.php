2bb35b52dcae30156617a0f1111f6f80487dc0c7
Vikas Kumar <VikashAmbani@users.noreply.github.com>
2019-06-07 13:14:20 +0530

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,36 +6,25 @@ use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use App\User;
 
 
 class DashboardController extends Controller {
 
     function __construct() {
-        //$this->middleware('auth:api');
+       // $this->middleware('auth:api');
     }
 
     public function index(Request $request) {
-        $resp=Response();
-        if ($request->ajax()) {
-            $view = view('dashboard.index',['isAjax'=>TRUE])->render();
-            return $resp->json(["status" => 'success','html' => $view, 'title' => 'Dashboard']);
-        } else {
-            return view('dashboard.index',['isAjax'=>FALSE]);
-        }
+           return view('dashboard.index',['isAjax'=>FALSE]);
+   
     }
     
     public function profile(Request $request) {
         $user = $request->user();
         $user = User::where('id', $user->user_id)->first()->toArray();
-        $resp=Response();
-        if ($request->ajax()) {
-            $view = view('profile.index', ['isAjax' => TRUE, 'user' => $user])->render();
-            return $resp->json(["status" => 'success', 'html' => $view, 'title' => 'Profile']);
-        } else {
+       
             return view('profile.index', ['isAjax' => FALSE, 'user' => $user]);
-        }
     }
 
 }
